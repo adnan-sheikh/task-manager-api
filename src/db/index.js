6da7749afe85c48ab21c5660a78da2c3730c3f60
Adnan Sheikh <adnan-sheikh@pop-os.localdomain>
2023-09-23 17:32:55 +0530

    create route for getting all the tasks and display in browser

---
+++ src/db/index.js
@@ -0,0 +1,150 @@
+let id = 0;
+
+const db = {
+  tasks: [
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+    {
+      id: id++,
+      title: "Complete Module 1, Node js Intro",
+      description:
+        "Module 1 was missed last week. Try finishing it before the next session",
+      completed: false,
+    },
+  ],
+};
+
+class MemDb {
+  static getTaskById(id) {
+    return db.tasks.find((task) => task.id === id);
+  }
+
+  static getAllTasks() {
+    return db.tasks;
+  }
+
+  static createNewTask(taskDetails) {
+    const newTask = { id: id++, taskDetails };
+    db.tasks.push(newTask);
+    return newTask;
+  }
+
+  static updateTask(taskDetails) {
+    const taskToUpdate = this.getTaskById(taskDetails.id);
+    if (!taskToUpdate) {
+      return null;
+    }
+    db[taskDetails.id] = taskToUpdate;
+    return taskToUpdate;
+  }
+
+  static deleteTaskById(id) {
+    db = db.tasks.filter((task) => task.id !== id);
+    return db.tasks;
+  }
+}
+
+module.exports = MemDb;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const path = require("path");
+const MemDb = require("./db");
 
 const PORT = 3000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  const indexFilePath = path.resolve("src", "pages", "index.html");
+  res.status(200).sendFile(indexFilePath);
+});
+
+app.get("/get-all-tasks", (req, res) => {
+  const getAllTasksFilePath = path.resolve(
+    "src",
+    "pages",
+    "get-all-tasks.html"
+  );
+  res.status(200).sendFile(getAllTasksFilePath);
+});
+
+app.get("/tasks", (req, res) => {
+  const allTasks = MemDb.getAllTasks();
+  res.status(200).json(allTasks);
+});
 
 app.listen(PORT, (err) => {
   if (err) {
