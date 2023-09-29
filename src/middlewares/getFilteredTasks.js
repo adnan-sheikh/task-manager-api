868f750b3d51880a12eca5e92939b9e8ad285673
Adnan Sheikh <adnan-sheikh@pop-os.localdomain>
2023-09-30 05:01:31 +0530

    refactor and add filtering feature based on title search

--- src/middlewares/getFilteredTasks.js
+++ src/middlewares/getFilteredTasks.js
@@ -1,18 +1,37 @@
+const { db } = require("../db");
+const { isEmpty } = require("../utils");
+
 function getFilteredTasks(req, res, next) {
   const queryParams = req.query;
   const completedFilter = queryParams?.completed;
+  const titleSearchQuery = queryParams?.title_search;
+  let filteredTasks = null;
+
   if (completedFilter === "true" || completedFilter === "false") {
-    const completed = completedFilter === "true";
-    const filteredTasks = db.tasks.filter(
-      (task) => task.completed === completed
+    const isTaskCompleted = completedFilter === "true";
+    filteredTasks = db.tasks.filter(
+      (task) => task.completed === isTaskCompleted
     );
-    if (filteredTasks.length === 0) {
-      return res.status(404).json({
-        message: "Tasks are empty! Try changing the filter!",
-      });
-    }
-    return res.status(200).json(filteredTasks);
   }
+
+  if (!isEmpty(titleSearchQuery)) {
+    filteredTasks = filteredTasks
+      ? filteredTasks.filter((task) =>
+          task.title.toLowerCase().includes(titleSearchQuery)
+        )
+      : db.tasks.filter((task) =>
+          task.title.toLowerCase().includes(titleSearchQuery)
+        );
+  }
+
+  if (filteredTasks?.length > 0) {
+    return res.status(200).json(filteredTasks);
+  } else if (filteredTasks?.length === 0) {
+    return res.status(404).json({
+      message: "Tasks are empty! Try changing the filter!",
+    });
+  }
+
   next();
 }
 
