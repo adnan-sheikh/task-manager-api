3e126945971cdc0ebcff6d76f41bb21087b49b7a
Adnan Sheikh <adnan-sheikh@pop-os.localdomain>
2023-09-30 07:00:47 +0530

    fix bugs which was there in v1 in v2

--- src/api/v2/handlers/tasks.js
+++ src/api/v2/handlers/tasks.js
@@ -1,6 +1,6 @@
 let { db, uniqueId } = require("../../../db");
 
-function getAllTasks(req, res) {
+function getAllTasks(_, res) {
   const allTasks = db.tasks;
   if (allTasks.length === 0) {
     return res.status(200).json([]);
@@ -12,49 +12,70 @@ function getTaskById(req, res) {
   const task = db.tasks.find((task) => task.id == req.params.id);
   if (!task) {
     return res.status(404).json({
-      message: "No such task found. Please try getting a task with valid ID!",
+      code: 4040,
+      message: "No such task found",
+      description: "Please try getting a task with valid ID!",
     });
   }
-  res.status(200).json({ ...task });
+  res.status(200).json(task);
 }
 
 function createNewTask(req, res) {
   const taskFromBody = req.body;
-  const newTask = { id: uniqueId++, ...taskFromBody, createdAt: Date.now() };
+  const { title, description, completed, priority } = taskFromBody;
+  const newTask = {
+    id: uniqueId++,
+    createdAt: Date.now(),
+    title,
+    description,
+    completed,
+    priority,
+  };
   db.tasks.push(newTask);
-  res
-    .status(201)
-    .json({ message: "Successfully created new task!", task: newTask });
+  res.status(201).json(newTask);
 }
 
 function updateTask(req, res) {
   const newTaskDetails = req.body;
-  const oldTaskDetails = db.tasks.find((task) => task.id == req.params.id);
+  const newTaskId = req.params.id;
+  const oldTaskDetails = db.tasks.find((task) => task.id == newTaskId);
   if (!oldTaskDetails) {
     return res.status(404).json({
-      message: "No such task found. Please try updating a task with valid ID!",
+      code: 4040,
+      message: "No such task found",
+      description: "Please try updating a task with valid ID!",
     });
   }
-  const taskId = +newTaskDetails.id;
-  const updatedTask = { ...newTaskDetails, id: taskId };
+  const taskId = +newTaskId;
+  const { title, description, completed, priority } = newTaskDetails;
+  const updatedTask = {
+    id: taskId,
+    createdAt: oldTaskDetails.createdAt,
+    title,
+    description,
+    completed,
+    priority,
+  };
   db.tasks = db.tasks.map((task) => {
     if (task.id == taskId) {
       return updatedTask;
     }
     return task;
   });
-  res.status(200).json({ ...updatedTask });
+  res.status(200).json(updatedTask);
 }
 
 function deleteTaskById(req, res) {
   const oldTaskDetails = db.tasks.find((task) => task.id == req.params.id);
   if (!oldTaskDetails) {
     return res.status(404).json({
-      message: "No such task found. Please try deleting a task with valid ID!",
+      code: 4040,
+      message: "No such task found",
+      description: "Please try deleting a task with valid ID!",
     });
   }
   db.tasks = db.tasks.filter((task) => task.id != req.params.id);
-  res.status(200).json({ message: "Successfully deleted the task!" });
+  res.status(200).json({ message: "success" });
 }
 
 module.exports = {
